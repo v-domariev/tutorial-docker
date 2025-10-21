12cf43e4916aa2512642a2e344ecf8e31015f3d5
Vladyslav Domariev <vlad.domariev@gmail.com>
2025-10-22 00:22:14 +0200

    Container port maping: part 2

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ https://docs.docker.com/compose/gettingstarted/
 # Tutorial by @kotojava on YT:
 https://www.youtube.com/watch?v=8OOUW1iX4xI&list=PLvjGcAdDu5utraOVR4akDAUthmgjVt0V5&index=8
 workplace dir: ./kotojava
+
+Lesson 4: Изучаем Docker. Понятие сети. 
+https://www.youtube.com/watch?v=aex-Spr8O1E&list=PLvjGcAdDu5utraOVR4akDAUthmgjVt0V5&index=4
--- kotojava/3_lesson_port_mapping/commands.sh
+++ kotojava/3_lesson_port_mapping/commands.sh
@@ -1,5 +1,8 @@
 echo "LINK: https://www.youtube.com/watch?v=4Tx8GnOjzy8&list=PLvjGcAdDu5utraOVR4akDAUthmgjVt0V5&index=5"
 echo "Commands:"
+
+echo 'docker run -dit --name mypython_server -p host_port/parent_port/external_port_container:internal_container_port/hosted_application_port_in_container python:3.9-slim; # -p or --publish
+'
 docker run -dit --name mypython_server -p 8888:8080 python:3.9-slim; # -p or --publish
 docker ps -a
 docker attach mypython_server
@@ -10,3 +13,4 @@ python -m http.server 8080
 apt update 
 apt install curl -y
 curl localhost:8080 # in container CLI
+curl localhost:8888 # in host/WindowsOS CLI/git_bash

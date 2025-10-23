d44d80f75cc8120391e1e33f9af0720ae5ec1aed
Vladyslav Domariev <vlad.domariev@gmail.com>
2025-10-23 21:28:31 +0200

    2 Containerize a .NET app: init infinity counter

---
+++ dotnet/2_containerize_dotnet_application/commands.sh
@@ -0,0 +1,8 @@
+#Link: https://learn.microsoft.com/en-us/dotnet/core/docker/build-container?tabs=windows&pivots=dotnet-9-0
+#Topic: 2 Tutorial: Containerize a .NET app
+dotnet new console -o App -n DotNet.Docker
+cd App
+dotnet run
+# Hello world
+#Rewrite code
+ dotnet run -- 5
---
+++ dotnet/2_containerize_dotnet_application_app/App/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Int32 counter = 0;
+
+Int32 max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+
+while(max is -1 || counter < max) 
+{
+	Console.WriteLine($"Counter: {++counter}");
+
+	await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}

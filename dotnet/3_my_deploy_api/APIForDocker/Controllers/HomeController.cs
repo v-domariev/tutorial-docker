ca58a7ad96c43c6b9ed57aa66410931065834e3d
Vladyslav Domariev <vlad.domariev@gmail.com>
2025-11-26 18:35:17 +0100

    Added HomePageController

---
+++ dotnet/3_my_deploy_api/APIForDocker/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIForDocker.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult GetHomePage() 
+        {
+            return Ok("It is a home page. My first test APP in WEB.");
+        }
+    }
+}
